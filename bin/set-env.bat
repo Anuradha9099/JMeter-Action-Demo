c0caa2b5bcded387732016ce364f1eb2db160d7a
AnuGitLab <piyumalanuradhapython@gmail.com>
2025-03-02 12:33:16 +0530

    Add jmx files

--- bin/set-env.bat
+++ bin/set-env.bat
@@ -1,4 +1,4 @@
-@rem set the \java_home\bin to the PATH
+@REM set the \java_home\bin to the PATH
 set JAVA_HOME=C:\Program Files\Java\jdk-11.0.0.2
 set PATH=%JAVA_HOME%\bin;%PATH%
 cd D:\JMeter_Traning\JMeter_Session\apache-jmeter-5.5\apache-jmeter-5.5\bin
